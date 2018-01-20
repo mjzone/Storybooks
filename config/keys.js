5ed62bdee2c75a88c40997acc05d6540889907a2
Manoj Fernando <mj.bitwave@gmail.com>
2018-01-20 13:25:41 +0530

    Adding google passport stategy

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb://manojf:lenovo@T520@ds211088.mlab.com:11088/storybook',
+  googleClientID: '988744594516-68oh94pa9ojj635omf3r0jfev56caipl.apps.googleusercontent.com',
+  googleClientSecret: 'IU_2dPt9YRDJbhBRqiTnFSIq'
+}
